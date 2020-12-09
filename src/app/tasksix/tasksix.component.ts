0abe1c4919da0b253bceb3b1414752bdfa4f8fbd
jegan1674@gmail.com <jegan1674@gmail.com>
2020-12-09 21:20:34 +0000

    task seven partially completed collapse not working.

--- src/app/tasksix/tasksix.component.ts
+++ src/app/tasksix/tasksix.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tasksix.component.css']
 })
 export class TasksixComponent implements OnInit {
-
+  public isCollapsed = true;
   constructor() { }
 
   ngOnInit(): void {
